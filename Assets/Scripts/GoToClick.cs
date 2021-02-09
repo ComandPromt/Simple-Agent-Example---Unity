e6994b212d207ddb8cdfd530163c50d7b2f27e6d
comandpromt <vayase_a_ferrol@hotmail.com>
2021-02-09 22:41:43 +0100

    v 1.0

---
+++ Assets/Scripts/GoToClick.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GoToClick : MonoBehaviour
+{
+	
+    Transform player;
+   
+    UnityEngine.AI.NavMeshAgent agent;
+
+    public Vector3 destinationPoint;
+  
+    private int vidaMaxima=100;
+	
+	private int vida;
+	 
+	public float speed = 10000;
+
+    void Start()
+    {
+		
+		vida=vidaMaxima;
+		
+        agent= GetComponent<UnityEngine.AI.NavMeshAgent>();
+		
+		agent.speed = speed;
+		
+        player = GameObject.FindGameObjectWithTag ("Obstaculo").transform;
+
+    }
+
+    void Update()
+    {
+
+		if((int)agent.transform.position.x==0){ 
+            	agent.SetDestination (new Vector3(-131, 60, -4));
+                
+        }
+
+        if((int)agent.transform.position.x==-131){ 
+            	agent.SetDestination (new Vector3(0, 60, 0));
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider obj) {
+     
+        if (obj.tag=="Player") {
+
+            --vida;
+			
+			Debug.Log("Vida: "+vida);
+			
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Personaje.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+
+    public float rotationSpeed = 10.0f;
+		
+    public float speed = 300f;
+	
+    public int vidaMaxima=100;
+
+    private int vidaActual;
+       
+    private float y,posicionYInicial,posicionZInicial,posicionXInicial;
+    
+	Vector3 posicionInicial;
+	
+	bool edificio;
+
+    private Rigidbody rigidBody;	
+	
+    void Start()
+    {
+				
+		Application.targetFrameRate = 60;
+		
+        rigidBody = GetComponent<Rigidbody>();
+		
+        vidaActual =  vidaMaxima;
+ 
+    }
+
+	void OnCollisionEnter(Collision other) {
+		
+		--vidaActual;
+		
+    }
+
+    void Update()
+    {
+				
+        float y = Input.GetAxis("Vertical") * speed*Time.deltaTime;
+		
+        float x = Input.GetAxis("Horizontal") * speed*Time.deltaTime;
+
+		transform.Translate(y,0.0f,-x);
+
+		// transform.Rotate(Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+							      
+    }
+		
+}
